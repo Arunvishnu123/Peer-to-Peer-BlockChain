ca05149327b7887291bcbb3a1cc7604856307ada
Arun R S <arunvishnu40@gmail.com>
2022-02-23 02:05:35 +0100

    transaction ledger added

--- Transaction/CreateTransactionRequest.py
+++ Transaction/CreateTransactionRequest.py
@@ -2,31 +2,21 @@ import hashlib
 import datetime
 
 
-class basicTransaction:
-    def __init__(self,senderName,recieverName,amount,message):
+class Transactions:
+    def __init__(self,senderName,recieverName,message):
         self.senderName = senderName
         self.recieverName =recieverName
-        self.amount = amount
         self.message=message
-
-    def createMessage(self):
-        data ={
-
-        }
-        data["amountname"]= self.amount
-        data["message"] = self.amount
-        return data
 
     def createTransaction(self):
         transaction ={
 
         }
-
         transactionIDCreation = str(self.senderName) + str(self.recieverName) + str(self.amount)
-        transaction["transactionID"]=hashlib.sha1(transactionIDCreation.encode()).hexdigest()
-        transaction["sendername"]= self.senderName
-        transaction["receivername"]= self.recieverName
-        transaction["data"]= self.createMessage()
-        transaction["dateandtime"]=str(datetime.datetime.now())
+        transaction["transactionID"] = hashlib.sha1(transactionIDCreation.encode()).hexdigest()
+        transaction["sendername"] = self.senderName
+        transaction["receivername"] = self.recieverName
+        transaction["message"] = self.message
+        transaction["dateandtime"] = str(datetime.datetime.now())
         return transaction
 
