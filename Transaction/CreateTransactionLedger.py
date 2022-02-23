eca7f9371a9b64f3eae6ef1b55cf34d4027caff3
Arun R S <arunvishnu40@gmail.com>
2022-02-23 02:16:13 +0100

    Updated transaction list module

--- Transaction/CreateTransactionLedger.py
+++ Transaction/CreateTransactionLedger.py
@@ -1,8 +1,13 @@
 class Transactions:
-    def _init_(self,transaction):
+    def _init_(self,transaction,mineRequest):
         self.transaction=transaction
+        self.mineRequest = mineRequest
+        self.transactionlist = []
     
-    def createtransactionlist(self):
-        transactionlist = []
-        transactionlist.append(self.transaction)
-        return transactionlist
+    def createtransactionlist(self):  
+        self.transactionlist.append(self.transaction)
+        if(self.mineRequest == 1):
+          return self.transactionlist
+          self.transactionlist.clear()
+          
+          
