26926d92d6ac44abf01ce412f176d8edff9935bf
Arun R S <arunvishnu40@gmail.com>
2022-03-18 08:30:32 +0100

    automatic connection using the livelness test of the registered peers with tracker is done but some changed are reuqired

---
+++ Tracker/Database/RegisteredPeers.py
@@ -0,0 +1,87 @@
+import sqlite3
+
+
+class RegisteredPeerTable:
+    def __init__(self):
+        pass
+
+    def createRegisteredTable(self):
+        db = sqlite3.connect('./Tracker/DatabaseSource/Tracker.db')
+        try:
+            cur = db.cursor()
+            cur.execute('''CREATE TABLE RegisteredPeers (        
+            peerIPAddress TEXT (20) NOT NULL,
+            peerPort INTEGER,
+            peerPublicKey TEXT (20) NOT NULL);''')
+            print('table created successfully')
+        except:
+            print('error in operation')
+            db.rollback()
+        db.close()
+
+    def addRegisteredElements(self,data):
+        db = sqlite3.connect('./Tracker/DatabaseSource/Tracker.db')
+        qry = "insert into RegisteredPeers (peerIPAddress,peerPort,peerPublicKey) values(?,?,?);"
+        try:
+            cur = db.cursor()
+            cur.execute(qry, data)
+            db.commit()
+            print("new peer details added to the database successfully")
+        except:
+            print("error in operation")
+            db.rollback()
+        db.close()
+
+    #retrive all ipaddress and port number
+    def retrieveRegisteredElements(self):
+        db = sqlite3.connect('./Tracker/DatabaseSource/Tracker.db')
+        qry = """select peerIPAddress , peerPort FROM RegisteredPeers """
+        try:
+            cur = db.cursor()
+            cur.execute(qry)
+            result = cur.fetchall()
+            db.close()
+            return result
+        except:
+            print("error in operation")
+
+    def retreieveRegisteredPeerName(self):
+        db = sqlite3.connect('./Tracker/DatabaseSource/Tracker.db')
+        qry = """select peerName FROM RegisteredPeers """
+        try:
+            cur = db.cursor()
+            cur.execute(qry)
+            result = cur.fetchall()
+            db.close()
+            return result
+        except:
+            print("error in operation")
+
+
+    def retrieveRegisteredAllSelected(self,name):
+        db = sqlite3.connect('./Tracker/DatabaseSource/Tracker.db')
+        qry = """select * FROM RegisteredPeers where peerName = ? """
+        try:
+            cur = db.cursor()
+            cur.execute(qry,(name,))
+            result = cur.fetchone()
+            db.close()
+            return result
+        except:
+            print("error in operation")
+
+    def deleteRegisteredPeerData(self,connection):
+        db = sqlite3.connect('./Tracker/DatabaseSource/Tracker.db')
+        qry = """delete FROM RegisteredPeers where peerIPAddress = ? """
+        try:
+           print("conencccdcdcdcdc",connection[0])
+           cur = db.cursor()
+           cur.execute(qry,(connection[0],))
+           db.commit()
+           db.close()
+           print("Successfully deleted")
+        except:
+           print("error in operation")
+
+
+
--- Trackermain.py
+++ Trackermain.py
@@ -4,6 +4,7 @@ from Tracker.Queue.QueueThreading import Queue
 from pathlib import Path
 from Tracker.Database.Creation import CreateDatabase
 from Tracker.Database.PeerDetails import PeerDetailsTable
+from Tracker.Database.RegisteredPeers import RegisteredPeerTable
 import time
 import socket
 
@@ -22,13 +23,16 @@ else:
 peerDataTable = PeerDetailsTable()
 peerDataTable.createTable()
 tracker = Tracker(connection)
+#Created Registered peers table
+createdRegisteredPeers = RegisteredPeerTable()
+createdRegisteredPeers.createRegisteredTable()
 ############################################################################################################
 #Automatic liviliness test for the peers
 def livelinesstest():
     while True:
        print("Doing Liveliness Test")
-       time.sleep(10)
-       connectionDetails = peerDataTable.retrieveElements()
+       time.sleep(40)
+       connectionDetails = createdRegisteredPeers.retrieveRegisteredElements()
        print("Connected peers list:",connectionDetails)
        y = tracker.liveness(connectionDetails,"00000")
        print("unconnected peers list:", y)
@@ -50,6 +54,7 @@ if __name__ == "__main__":
             extractReceivedData = tracker.extractIP()
             print(extractReceivedData)
             peerDataTable.addElements(extractReceivedData)
+            createdRegisteredPeers.addRegisteredElements(extractReceivedData)
             connectionDetails = peerDataTable.retrieveElements()
             tracker.sendNewNode(connectionDetails, receivedMessage)
 
